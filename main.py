4f5dd613ce3f9f09621f56713366760c92d19939
neolicca <70485228+neolicca@users.noreply.github.com>
2020-08-30 23:59:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import tweepy
+import time
+import random
+
+def main():
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler("1z3rQObOuiRmnIG6ywLLsQN6h",
+                            "WXCoj5Eh8k2ZhslDP7mmB7ESpnrxKyBHf8G4D0580lVPfrCrHw")
+    auth.set_access_token("1011239315895013380-K3tac7dAIc8xaSlW96osSZRNbangWA",
+                        "IYXiMB9y8Gycmw7wO6WL7Bzw4ceROB8rocAQOSii5QDgB")
+
+    # Create API object
+    api = tweepy.API(auth)
+
+    # Create a tweet
+    while True:
+        random_number = random.randint(1,4)
+        image_path = f'gifs/{random_number}.gif'
+        api.update_with_media(image_path,'')
+        print('Uploaded')
+        time.sleep(20)
+
+if __name__ == "__main__":
+    main()
+    
